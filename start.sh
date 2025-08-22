d9d894ef14492dbd30457d8b526ae3c17d57a44c
thomas1001-droid <thomas.beaupertuis@1001pneus.fr>
2025-08-22 09:45:03 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
 set -e
 
-# Génération du fichier sasl_passwd si variables fournies
-if [ -n "$RELAYHOST" ] && [ -n "$RELAYHOST_USER" ] && [ -n "$RELAYHOST_PASSWORD" ]; then
-  echo "[$RELAYHOST]:587 $RELAYHOST_USER:$RELAYHOST_PASSWORD" > /etc/postfix/sasl_passwd
-  postmap /etc/postfix/sasl_passwd
-  chmod 600 /etc/postfix/sasl_passwd /etc/postfix/sasl_passwd.db
-fi
-
-# Lancer Postfix
 exec /usr/sbin/postfix start-fg
